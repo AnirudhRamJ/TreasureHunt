9c4813a87b9eaba8eac5157591fa48a81e18fe64
AnirudhRamJ <74857186+AnirudhRamJ@users.noreply.github.com>
2021-07-09 19:08:57 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+
+    fill("white");
+    textSize(15);
+    text("T I O C N F U N", 100,250);
+    fill("lightblue");
+    text("Hint: Performs a task !!", 100,270);
+
+    fill("white");
+    textSize(15);
+    text("B A T A D S A E", 700,150);
+    fill("lightblue");
+    text("Hint: Stores information !!", 700,170);
+    
+
+}
